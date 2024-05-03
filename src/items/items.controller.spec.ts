a8f3673458651150dfef49246aec8541c464af9c
Nomi <naumanahmed19@gmail.com>
2024-05-03 21:29:24 +0200

    test(items): controller unit test

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -1,20 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { Item } from './entities/item.entity';
 
 describe('ItemsController', () => {
-  let controller: ItemsController;
+  let itemsController: ItemsController;
+  let itemsService: ItemsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ItemsController],
-      providers: [ItemsService],
+      providers: [
+        {
+          provide: ItemsService,
+          useValue: {
+            findAll: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<ItemsController>(ItemsController);
+    itemsController = module.get<ItemsController>(ItemsController);
+    itemsService = module.get<ItemsService>(ItemsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should call findAll method from ItemsService', async () => {
+    // Arrange
+    const mockItems: Item[] = [
+      {
+        id: 1, name: 'Item 1',
+        description: undefined,
+        parent: undefined
+      },
+      {
+        id: 2, name: 'Item 2',
+        description: undefined,
+        parent: undefined
+      },
+    ];
+    itemsService.findAll = jest.fn().mockResolvedValue(mockItems);
+
+    await itemsController.findAll();
+    expect(itemsService.findAll).toHaveBeenCalled();
   });
 });
